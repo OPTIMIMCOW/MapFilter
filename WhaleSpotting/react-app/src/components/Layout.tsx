a736fa784890c6ba04d597f6c6adec3bd33d0fd2
Oskar Williams <oskarwilliams378@gmail.com>
2021-07-21 17:48:17 +0100

    Initial commit

---
+++ WhaleSpotting/react-app/src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+export default function Counter() {
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <div>
+            <h1>Counter</h1>
+
+            <p>This is a simple example of a React component.</p>
+
+            <p aria-live="polite">Current count: <strong>{counter}</strong></p>
+
+            <button className="btn btn-primary" onClick={() => setCounter(counter + 1)}>Increment</button>
+        </div>
+    );
+}
