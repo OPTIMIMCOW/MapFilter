db945e6ae2be020c6031931c7ec1311d6a6ad398
Oskar Williams <oskarwilliams378@gmail.com>
2021-07-22 12:32:04 +0100

    Add basic tests

---
+++ WhaleSpotting.UnitTests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using WhaleSpotting.Controllers;
+using Xunit;
+
+namespace WhaleSpotting.UnitTests.Controllers
+{
+    public class WeatherForecastControllerTests
+    {
+        private readonly WeatherForecastController _underTest;
+        
+        public WeatherForecastControllerTests()
+        {
+            _underTest = new WeatherForecastController();
+        }
+
+        [Fact]
+        public void Get_WhenCalled_Returns5Models()
+        {
+            // Act
+            var result = _underTest.Get();
+            
+            // Assert
+            result.Should().HaveCount(5);
+        }
+    }
+}
--- WhaleSpotting/Controllers/WeatherForecastController.cs
+++ WhaleSpotting/Controllers/WeatherForecastController.cs
@@ -16,13 +16,6 @@ namespace WhaleSpotting.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
         public IEnumerable<WeatherForecastApiModel> Get()
         {
