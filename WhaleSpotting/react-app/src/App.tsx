52e210f786ed9a2d3ed5c4fb314708524c19348d
Ismat <ismat.mahmood@softwire.com>
2021-07-27 11:11:58 +0100

    Initial App Routing done

--- WhaleSpotting/react-app/src/App.tsx
+++ WhaleSpotting/react-app/src/App.tsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import { Route } from 'react-router';
+import { useContext } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import Counter from "./components/Counter";
 import FetchData from "./components/FetchData";
-import { BrowserRouter } from 'react-router-dom';
+import { Nav } from 'reactstrap';
+
+
 
 function App() {
+
     return (
-        <BrowserRouter>
-            <Layout>
-              <Route exact path='/' component={Home} />
-              <Route path='/counter' component={Counter} />
-              <Route path='/fetch-data' component={FetchData} />
-            </Layout>
-        </BrowserRouter>
+        <Router>
+            <Nav />
+                <Switch>
+                    <Route exact path='/'  />
+                    <Route path='/map'  />
+                    <Route path='/reportsighting'  />
+                    <Route path='/profile'  />
+                    <Route path='/login'  />
+                    <Route path='/register'  />
+                    <Route path="*">
+                        <Home />
+                    </Route>
+                </Switch >
+            <Footer />
+        </Router >
     );
 }
-
 export default App;
