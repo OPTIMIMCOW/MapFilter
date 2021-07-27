c084881750fd6bfbcf456ae2f8bdbcd95c21fb31
Ismat <ismat.mahmood@softwire.com>
2021-07-27 12:26:39 +0100

    Fixed Errors and test

--- WhaleSpotting/react-app/src/App.test.tsx
+++ WhaleSpotting/react-app/src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import Home from './components/Home';
 
 test('renders Home Page', () => {
-  render(<App />);
-  expect(<Home />).toBeDefined();
+    render(<App />);
+    const linkElement = screen.getByText(/ASP.NET Core/i);
+    expect(linkElement).toBeInTheDocument();
 });
--- WhaleSpotting/react-app/src/App.tsx
+++ WhaleSpotting/react-app/src/App.tsx
@@ -1,31 +1,20 @@
-import { useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
-import Layout from "./components/Layout";
 import Home from "./components/Home";
-import Counter from "./components/Counter";
-import FetchData from "./components/FetchData";
-import { Nav } from 'reactstrap';
-
-
 
 function App() {
-
     return (
         <Router>
-            <Nav />
-                <Switch>
-                    <Route exact path='/' component={Home}  />
-                    <Route path='/map'  />
-                    <Route path='/reportsighting'  />
-                    <Route path='/profile'  />
-                    <Route path='/login'  />
-                    <Route path='/register'  />
-                    <Route path="*">
-                        <Home />
-                    </Route>
-                </Switch >
+            <Switch>
+                <Route path='/map' />
+                <Route path='/reportsighting' />
+                <Route path='/profile' />
+                <Route path='/login' />
+                <Route path='/register' />
+                <Route path='' component={Home} />
+            </Switch >
         </Router >
     );
 }
+
 export default App;
