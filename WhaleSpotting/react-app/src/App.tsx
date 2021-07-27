3b205894c0452e15d78d51e8d2192578ee02fc07
Ilyas <ilyas.ali@softwire.com>
2021-07-27 12:00:48 +0100

    First commit for skeleton page

--- WhaleSpotting/react-app/src/App.tsx
+++ WhaleSpotting/react-app/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
             <Layout>
               <Route exact path='/' component={Home} />
               <Route path='/counter' component={Counter} />
-              <Route path='/fetch-data' component={FetchData} />
+              {/* <Route path='/fetch-data' component={FetchData} /> */}
             </Layout>
         </BrowserRouter>
     );
