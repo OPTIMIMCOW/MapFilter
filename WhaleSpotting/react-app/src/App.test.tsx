60bae7264b63e9523f0aa763f14e5d3f6a753914
Ilyas <ilyas.ali@softwire.com>
2021-07-27 15:44:44 +0100

    Skeleton Landing page with tests

--- WhaleSpotting/react-app/src/App.test.tsx
+++ WhaleSpotting/react-app/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("Renders Home Page", () => {
     render(<App />);
-    const linkElement = screen.getByText(/ASP.NET Core/i);
+    const linkElement = screen.getByTestId("home");
     expect(linkElement).toBeInTheDocument();
-});
+})
---
+++ WhaleSpotting/react-app/src/tests/Home.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../components/Home";
+
+describe("Home Page tests", () => {
+    test("Renders Recent Sightings", () => {
+        render(<Home />);
+        const text = screen.getByText("Recent Sightings");
+        expect(text).toBeInTheDocument();
+    })
+
+    test("Renders Filter button", () => {
+        render(<Home />);
+        const filter = screen.getByTestId("filter-button");
+        expect(filter).toBeInTheDocument();
+    })
+
+    test("Renders page nav", () => {
+        render(<Home />);
+        const pageNav = screen.getByTestId("page-nav");
+        expect(pageNav).toBeInTheDocument();
+    })
+})
