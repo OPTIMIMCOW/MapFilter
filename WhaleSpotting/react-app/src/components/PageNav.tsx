1d3eb0c5fd089c1f318bc747f6d31101a5e22d73
Lena E <elena.evdokimova@softwire.com>
2021-07-28 12:27:18 +0100

    fixed

--- WhaleSpotting/react-app/src/components/PageNav.tsx
+++ WhaleSpotting/react-app/src/components/PageNav.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/PageNav.scss";
 
-export default function PageNav() : JSX.Element{
+export default function PageNav() : JSX.Element {
     return (
         <div className="page-nav" data-testid="page-nav">
             <button className="page-button"> Previous Page</button>
