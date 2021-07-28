316d23c31ec438c7c453d8bf7ee008e94e5a6fa7
OPTIMIMCOW <kenallan18@hotmail.com>
2021-07-28 21:28:01 +0100

    addressed all but 1 comment. Removed redundant style attributes.

--- WhaleSpotting/react-app/src/components/Profile.tsx
+++ WhaleSpotting/react-app/src/components/Profile.tsx
@@ -1,30 +1,30 @@
 import "../styles/Profile.scss";
 import "../styles/Home.scss";
+import "../styles/Buttons.scss";
 import React from "react";
 import { useState } from "react";
 import PageNav from "./PageNav";
 
 export function Profile(): JSX.Element {
-
     const [feedToggle, setFeedToggle] = useState("Sightings");
 
     return (
         <div className="body">
-            <div className="banner">
+            <div className="profile-pane">
                 <div className="outer-container">
                     <div className="trophy-text-container">
                         <h1 className="heading">UserName</h1>
                         <p className="joined little-text"> Joined: June 2004</p>
                         <div className="trophy-container">
                             <p className="feature-text"> 15</p>
-                            <p className="reported little-text"> Reported <br></br> Sightings</p>
-                            <img className="trophy-image" src="https://picsum.photos/id/215/70" />
+                            <p className="reported little-text"> Reported <br/> Sightings</p>
+                            <img className="trophy-image" alt="Trophy Image" src="https://picsum.photos/id/215/50" />
                         </div>
-                        <img className="profile-image" src="https://picsum.photos/id/237/200" />
+                        <img className="profile-image" alt="Profile Image" src="https://picsum.photos/id/237/200" />
                     </div>
                     <div className="button-container">
-                        <a className="toggle" onClick={() => setFeedToggle("Sightings")}>Sightings</a>
-                        <a className="toggle" data-testid="approval-toggle" onClick={() => setFeedToggle("Approvals")}>Approvals</a>
+                        <button className="primary-button" onClick={() => setFeedToggle("Sightings")}>Sightings</button>
+                        <button className="primary-button" data-testid="approval-toggle" onClick={() => setFeedToggle("Approvals")}>Approvals</button>
                     </div>
                 </div>
             </div>
