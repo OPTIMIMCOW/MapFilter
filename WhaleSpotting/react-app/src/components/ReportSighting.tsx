8e30923f9e9db58712249818ecb1b23ca66ea5f2
Ismat <ismat.mahmood@softwire.com>
2021-07-29 14:48:28 +0100

    Skeleton Report Sightings page

--- WhaleSpotting/react-app/src/components/ReportSighting.tsx
+++ WhaleSpotting/react-app/src/components/ReportSighting.tsx
@@ -1,44 +1,57 @@
 import "../styles/ReportSighting.scss";
 import React from "react";
 
-
 export default function ReportSighting(): JSX.Element {
     return (
         <div className="report-sighting" data-testid="report-sighting">
             <img className="banner-image" src="https://ssl.tzoo-img.com/images/tzoo.1.0.685904.shutterstock_567677317.jpg" alt="whale" />
-
-            <div className="sighting-contents">
-                <div className="sightings-header">
-                    <h2>Report Your Sighting</h2>
-                </div>
-
-                <div className="sighting-holder">
-
-                    <form>
-                        <div className="card-component">
-                            <div className="left">
-                                <input type="date" name="sighting-date-time" placeholder="Sighting Date Time" ></input>
-                                <input type="text" name="species" placeholder="Species" ></input>
-                                <input type="number" name="quantity" placeholder="Quantity" ></input>
-                                <input type="text" name="longitude" placeholder="Longitude" ></input>
+            <div className="container">
+                <div className="title">Report Your Sighting</div>
+                <form action="#">
+                    <div className="card-component">
+                        <div className="sighting-details">
+                            <div className="input-box">
+                                <label className="details">Sighting Date Time</label>
+                                <input type="date" placeholder="Enter Sighting Date Time" required></input>
                             </div>
-                            <div className="right">
-                                <input type="text" name="location" placeholder="Location" ></input>
-                                <input type="text" name="orca-type" placeholder="Ocra Type" ></input>
-                                <input type="text" name="orca-pod" placeholder="Ocra Pod" ></input>
-                                <input type="text" name="latitude" placeholder="Latitude" ></input>
+                            <div className="input-box">
+                                <label className="details">Location</label>
+                                <input type="text" placeholder="Confirm your location" required></input>
                             </div>
-                            <div>
-                                <input type="textarea" name="description" placeholder="Description" ></input>
-                                <input type="text" name="image-url" placeholder="Image url" ></input>
+                            <div className="input-box">
+                                <label className="details">Species</label>
+                                <input type="text" placeholder="Enter species" required></input>
+                            </div>
+                            <div className="input-box">
+                                <label className="details">Quantity</label>
+                                <input type="number" placeholder="Enter quantity" ></input>
+                            </div>
+                            <div className="input-box">
+                                <label className="details">Longitude</label>
+                                <input type="number" placeholder="Enter your longitude" ></input>
+                            </div>
+                            <div className="input-box">
+                                <label className="details">Latitude</label>
+                                <input type="number" placeholder="Enter your latitude" ></input>
+                            </div>
+                            <div className="input-box">
+                                <label className="details">Orca Pod</label>
+                                <input type="text" placeholder="Orca Pod" ></input>
+                            </div>
+                            <div className="input-box">
+                                <label>Image Url</label>
+                                <input type="url" placeholder="Image url"></input>
+                            </div>
+                            <div className="input-box">
+                                <label>Description</label>
+                                <input type="textarea" placeholder="Description"></input>
                             </div>
                         </div>
-                        <div className="button">
-                            <input type="submit" value="Submit"></input>
-                        </div>
-                    </form>
-
-                </div>
+                    </div>
+                    <div className="button">
+                        <input type="submit" value="Submit Sighting"></input>
+                    </div>
+                </form>
             </div>
         </div>
     );
