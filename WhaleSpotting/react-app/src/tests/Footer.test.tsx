8483b462ce8f78d52ab438f7a5120e012eaaf1fa
Francesca Stocco <francescaj.stocco@gmail.com>
2021-07-29 12:31:41 +0100

    review comments addressed
    changed tests & styling

--- WhaleSpotting/react-app/src/tests/Footer.test.tsx
+++ WhaleSpotting/react-app/src/tests/Footer.test.tsx
@@ -5,17 +5,17 @@ import React from "react";
 test("renders Whale Museum logo in footer", () => {
     render(<Footer />);
 
-    const whaleMuseumLogo = screen.getByTestId("whale-museum-logo");
+    const whaleMuseumLogo = screen.getByTestId("whale-museum-logo") as HTMLImageElement;
 
     expect(whaleMuseumLogo).toBeInTheDocument();
-    expect(whaleMuseumLogo.src === "https://cdn.shopify.com/s/files/1/0249/1083/t/3/assets/wm-logo-sm.png").toBeTruthy();
+    expect(whaleMuseumLogo.src).toBe("https://cdn.shopify.com/s/files/1/0249/1083/t/3/assets/wm-logo-sm.png");
 });
 
 test("renders TechSwitch logo in footer", () => {
     render(<Footer />);
 
-    const techSwitchLogo = screen.getByTestId("techswitch-logo");
+    const techSwitchLogo = screen.getByTestId("techswitch-logo") as HTMLImageElement;
 
     expect(techSwitchLogo).toBeInTheDocument();
-    expect(techSwitchLogo.src === "https://www.techswitch.co.uk/images/logo.svg").toBeTruthy();
+    expect(techSwitchLogo.src).toBe("https://www.techswitch.co.uk/images/logo.svg");
 });
