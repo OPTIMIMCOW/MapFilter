1ede484ea3065996a0740b92153e5f8d391d2d6f
Francesca Stocco <francescaj.stocco@gmail.com>
2021-07-29 11:52:46 +0100

    Create tests & added nested styling

---
+++ WhaleSpotting/react-app/src/tests/Footer.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { Footer } from "../components/Footer";
+
+test("renders Whale Museum logo in footer", () => {
+    render(<Footer />);
+
+    const whaleMuseumLogo = screen.getByTestId("whale-museum-logo");
+
+    expect(whaleMuseumLogo).toBeInTheDocument();
+    expect(whaleMuseumLogo.src === "https://cdn.shopify.com/s/files/1/0249/1083/t/3/assets/wm-logo-sm.png").toBeTruthy();
+});
+
+test("renders TechSwitch logo in footer", () => {
+    render(<Footer />);
+
+    const techSwitchLogo = screen.getByTestId("techswitch-logo");
+
+    expect(techSwitchLogo).toBeInTheDocument();
+    expect(techSwitchLogo.src === "https://www.techswitch.co.uk/images/logo.svg").toBeTruthy();
+});
