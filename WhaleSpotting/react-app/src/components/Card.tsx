8d8bb39ecb106e18bee139b5d193bb7988a4826b
Lena E <elena.evdokimova@softwire.com>
2021-07-29 17:50:10 +0100

    Card component created
    Need to write some tests.

--- WhaleSpotting/react-app/src/components/Card.tsx
+++ WhaleSpotting/react-app/src/components/Card.tsx
@@ -1,26 +1,34 @@
 import "../styles/Card.scss";
-import React from "react";
-// import PageNav from "./PageNav";
+import React, { useState } from "react";
 
-export default function Card(): JSX.Element {
+export default function Card() : JSX.Element {
 
+    const [closeCard, setCardState] = useState(true);
+
+    // Admin and approved will be received from backend.
+    const admin = true;
+    const approved = false;
+    
     return (
-        <div className="card-component" data-testid="card">
-            <div className="first-column columns">
-                <div className="always">Sighted At: 21 Aug 2020</div>
-                <div className="always">Species: Orca</div>
-                <div className="always">Location: </div>
-                <div className="always">Quantity: 3</div>
-                <div className="always">Reported By: Whale Watcher </div>
+        <div className="card-component">
+            <div className={admin && !approved ? "admin pending" : "non-admin pending"}> pending </div>
+            <div className="card-info"
+                onClick={() => setCardState(!closeCard)} data-testid="card">
+                <div className={closeCard ? "first-column" : "first-column open"}>
+                    <div className="always">Sighted At: 21 Aug 2020</div>
+                    <div className="always">Species: Orca</div>
+                    <div className="always">Location: </div>
+                    <div className="always">Quantity: 3</div>
+                    <div className="always">Reported By: Whale Watcher </div>
+                </div>
+                <div className={closeCard ? "second-column" : "second-column open"}>
+                    <div className="desktop">Orca type: Orca</div>
+                    <div className="desktop">Longitude: </div>
+                    <div className="desktop">Latitude </div>
+                    <div className="admin">User Id: 1</div>
+                    <div className="desktop">Description: Whales are fully aquatic, open-ocean creatures </div>
+                </div>
             </div>
-            <div className="second-column columns">
-                <div className="desktop">Orca type: Orca</div>
-                <div className="desktop">Longitude: </div>
-                <div className="desktop">Latitude </div>
-                <div className="admin">User Id: 1</div>
-                <div className="desktop">Description: Wdfjslfj sdfkjaslfdjka lsadfjkdsf Wdfjsl fjWdfjslfj Wdfjslf jWdfj slfjWdf jslfj sldfjasf lasdfkjsdaf asdlfsadal </div>
-            </div>
-            <div className="admin pending"> pending </div>
         </div>
     );
 }
