d74613b1a613a266bf909f1f936efd116ba13567
OPTIMIMCOW <kenallan18@hotmail.com>
2021-07-30 22:31:57 +0100

    Added object type to the api test.

--- WhaleSpotting/react-app/src/tests/CreateSightingApiModel.test.ts
+++ WhaleSpotting/react-app/src/tests/CreateSightingApiModel.test.ts
@@ -20,14 +20,14 @@ test("check species enum works correctly", () => {
 });
 
 test("check orcaType nullable enum works correctly", () => {
-    const test = {
+    const test: CreateSightingApiModel = {
         species: Species.AtlanticWhiteSidedDolphin,
         quantity: 2,
         description: "was nice",
         longitude: -100.010,
         latitude: -22.010,
         location: "atlantic ocean",
-        sightedAt: Date.now,
+        sightedAt: new Date('July 30, 2021 16:00:00'),
         orcaType: null,
         orcaPod: "",
         userId: 5,
