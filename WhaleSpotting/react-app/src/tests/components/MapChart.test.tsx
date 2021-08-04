4e37931e96a94e7ebe9de46f7d7580df86bd1e4a
Lena E <elena.evdokimova@softwire.com>
2021-08-04 14:16:12 +0100

    Test fixed

--- WhaleSpotting/react-app/src/tests/components/MapChart.test.tsx
+++ WhaleSpotting/react-app/src/tests/components/MapChart.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import MapChart from "../../components/MapChart";
+import { MapChart } from "../../components/MapChart";
 import { BrowserRouter as Router } from "react-router-dom";
 
 describe("Map Chart component tests", () => {
