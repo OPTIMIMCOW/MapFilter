ca9a6eaf0d828e689b8757f0d29ed83d9e3389d0
Suwanna <suwanna.bgu@gmail.com>
2021-08-05 11:50:00 +0100

    Added SearchSightingRequestModel

---
+++ WhaleSpotting/Models/RequestModels/SearchSightingRequestModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WhaleSpotting.Models.Enums;
+
+namespace WhaleSpotting.Models.RequestModels
+{
+    public class SearchSightingRequestModel
+    {
+        [Range(1, 16, ErrorMessage = "Species must be between 1 and 16")]
+        public Species Species { get; set; }
+
+        [Range(-180.00, 180.00, ErrorMessage = "Longitude must be between -180 and 180 degrees")]
+        public double Longitude { get; set; }
+
+        [Range(-90.00, 90.00, ErrorMessage = "Latitude must be between -90 and 90 degrees")]
+        public double Latitude { get; set; }
+
+        public string Location { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "This field is recieving date format: dd/mm/yyyy")]
+        public DateTime SightedAt { get; set; }
+
+        [Range(1, 4, ErrorMessage = "OrcaType must be between 1 and 4")]
+        public OrcaType? OrcaType { get; set; }
+
+        public string OrcaPod { get; set; }
+    }
+}
