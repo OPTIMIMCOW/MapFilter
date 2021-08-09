641f4795b68697a44c1c878e78514a41e4e17c4a
Lena E <elena.evdokimova@softwire.com>
2021-08-09 15:06:09 +0100

    Linting fixed

--- WhaleSpotting/react-app/src/api/apiClient.ts
+++ WhaleSpotting/react-app/src/api/apiClient.ts
@@ -1,11 +1,10 @@
-﻿import React from "react";
-import { SightingApiModel } from "./models/SightingApiModel";
+﻿import { SightingApiModel } from "./models/SightingApiModel";
 import authService from "../components/api-authorization/AuthorizeService";
 
 export async function fetchPendingSightings(pageNumber: number): Promise<SightingApiModel[]> {
     const token = await authService.getAccessToken();
     return await fetch(`https://localhost:5001/api/sightings/pending?page=${pageNumber}&pageSize=10`, {
         headers: !token ? {} : { "Authorization": `Bearer ${token}` }
-        })
+    })
         .then(r => r.json());
 }
