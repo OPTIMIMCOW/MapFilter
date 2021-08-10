252adcdd673c821575a3474f8baa724225dbf2c8
Francesca Stocco <francescaj.stocco@gmail.com>
2021-08-10 11:17:15 +0100

    Created User Response Model and get endpoint in controller

---
+++ WhaleSpotting/Models/ResponseModels/UserResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhaleSpotting.Models.DbModels;
+
+namespace WhaleSpotting.Models.ResponseModels
+{
+    public class UserResponseModel
+    {
+        public string Username { get; set; }
+        public IList<SightingResponseModel> Sightings { get; set; }
+
+        public UserResponseModel(UserDbModel user)
+        {
+            Username = user.UserName;
+            Sightings = user.Sightings.Select(s => new SightingResponseModel(s)).ToList();
+        }
+    }
+}
