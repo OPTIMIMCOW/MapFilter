bb118bcfc9716254b74b9251e7189a502c3f90d3
Ilyas <ilyas.ali@softwire.com>
2021-08-11 14:08:39 +0100

    Banner to map page

--- WhaleSpotting/react-app/src/components/Map.tsx
+++ WhaleSpotting/react-app/src/components/Map.tsx
@@ -3,6 +3,7 @@ import "../styles/Home.scss";
 import React, { useState } from "react";
 import { MapChart } from "./MapChart";
 import SightingMapInfo from "./SightingMapInfo";
+import { BannerImage } from "./BannerImage";
 
 export interface Chosen {
     id: number,
@@ -15,13 +16,16 @@ export default function Map(): JSX.Element {
     const [chosen, setChosen] = useState<Chosen>();
 
     return (
-        <div className="map-component" data-testid="map-component">
-            <h2 className="map-header">Map of Sightings</h2>
-            <div className="map-container" data-testid="map-container">
-                <MapChart chosen={chosen} setChosen={setChosen} />
-            </div>
-            <div className="map-info" data-testid="map-info">
-                <SightingMapInfo chosen={chosen} />
+        <div>
+            <BannerImage />
+            <div className="map-component" data-testid="map-component">
+                <h2 className="map-header">Map of Sightings</h2>
+                <div className="map-container" data-testid="map-container">
+                    <MapChart chosen={chosen} setChosen={setChosen} />
+                </div>
+                <div className="map-info" data-testid="map-info">
+                    <SightingMapInfo chosen={chosen} />
+                </div>
             </div>
         </div>
     );
