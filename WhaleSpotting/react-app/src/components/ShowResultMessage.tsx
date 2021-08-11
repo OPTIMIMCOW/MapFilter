11bdab3a0321cd94754801f975945d66329ea0b4
OPTIMIMCOW <kenallan18@hotmail.com>
2021-08-11 16:16:55 +0100

    Didnt save properly.

--- WhaleSpotting/react-app/src/components/ShowResultMessage.tsx
+++ WhaleSpotting/react-app/src/components/ShowResultMessage.tsx
@@ -7,7 +7,7 @@ interface ShowResultMessageProps {
 
 export default function ShowResultMessage({ responseMessage, showForm }: ShowResultMessageProps): JSX.Element {
     return responseMessage
-        ? <p data-testid="response-result" className={showForm ? "response-message-card" : "error-background"  }>
+        ? <p data-testid="response-result" className={showForm ? "response-message-card" : "error-background"}>
             {responseMessage}
         </p>
         : <div />;
