4a585b907232a48bfda38a853d245e965fe692bf
OPTIMIMCOW <kenallan18@hotmail.com>
2021-08-26 12:21:50 +0100

    Got batching working using useeffect.

--- WhaleSpotting/react-app/src/components/MapChart.tsx
+++ WhaleSpotting/react-app/src/components/MapChart.tsx
@@ -12,6 +12,7 @@ import {
 import { Chosen } from "./Map";
 import { fetchBatchSightings } from "../api/apiClient";
 import { BatchSightingRequestModel } from "../api/models/BatchSightingRequestModel";
+import update from 'immutability-helper';
 
 const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
@@ -25,57 +26,52 @@ export function MapChart({ chosen, setChosen }: MapChartProps): JSX.Element {
     const [batch, setBatch] = useState<number>(0);
 
     useEffect(() => {
-    }, []);
+        if (batch < 10) {
+            const request: BatchSightingRequestModel = {
+                maxLatitude: -72 + (18 * batch),
+                minLatitude: -90 + (18 * batch),
+                batchNumber: batch + 1,
+            };
 
-    function batchLoad(): void {
-        const request: BatchSightingRequestModel = {
-            maxLatitude: -45 + (45 * batch),
-            minLatitude: -90 + (45 * batch),
-            batchNumber: batch + 1,
-        };
+            fetchBatchSightings(request)
+                .then(newData => {
+                    const changeData = data.concat(newData);
+                    setBatch(batch + 1);
+                    setData(changeData);
+                });
+        }
+    }, [data]);
 
-        fetchBatchSightings(request)
-            .then(newData => setData(data.concat(newData)));
-
-        setBatch(batch + 1);
+    if (data.length === 0) {
+        return <div data-testid="loading"> Loading... </div>;
     }
 
-    //if (data.length === 0) {
-    //    return <div data-testid="loading"> Loading... </div>;
-    //}
-
-    console.log("the component re ran");
-    console.log(data);
-
     return (
-        <div>
-            <button onClick={() => batchLoad()}> Batch Number:{batch}</button>
-            <ComposableMap
-                projection="geoEqualEarth"
-                data-testid="simple-map">
-                <ZoomableGroup zoom={1}>
-                    <Geographies geography={geoUrl}>
-                        {({ geographies }) =>
-                            geographies.map(geo => <Geography
-                                key={geo.rsmKey}
-                                geography={geo}
-                                fill="#DDD"
-                                stroke="#FFF"
-                            />)
-                        }
-                    </Geographies>
-                    {data.map(({ id, longitude, latitude }, index) => {
-                        const isChosen = chosen !== undefined && id === chosen.id;
-                        return <Marker
-                            data-testid={isChosen ? "chosen" : "not-chosen"}
-                            key={index} coordinates={[longitude, latitude]} name=""
-                            onClick={() => setChosen({ id: id, lat: latitude, lon: longitude })} >
-                            <circle r={2} fill={isChosen ? "#FFA500" : "#0000FF"} stroke="#fff" strokeWidth={0.2} />
-                        </Marker>;
+        <ComposableMap
+            projection="geoEqualEarth"
+            data-testid="simple-map">
+            <ZoomableGroup zoom={1}>
+                <Geographies geography={geoUrl}>
+                    {({ geographies }) =>
+                        geographies.map(geo => <Geography
+                            key={geo.rsmKey}
+                            geography={geo}
+                            fill="#DDD"
+                            stroke="#FFF"
+                        />)
                     }
-                    )}
-                </ZoomableGroup>
-            </ComposableMap>
-        </div>
+                </Geographies>
+                {data.map(({ id, longitude, latitude }, index) => {
+                    const isChosen = chosen !== undefined && id === chosen.id;
+                    return <Marker
+                        data-testid={isChosen ? "chosen" : "not-chosen"}
+                        key={index} coordinates={[longitude, latitude]} name=""
+                        onClick={() => setChosen({ id: id, lat: latitude, lon: longitude })} >
+                        <circle r={2} fill={isChosen ? "#FFA500" : "#0000FF"} stroke="#fff" strokeWidth={0.2} />
+                    </Marker>;
+                }
+                )}
+            </ZoomableGroup>
+        </ComposableMap>
     );
 }
